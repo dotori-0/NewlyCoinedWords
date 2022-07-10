371ceb7d0350805419134e8098432d73f926d960
dotori-0 <sesan131@gmail.com>
2022-07-10 17:24:45 +0900

    Initial Commit

---
+++ NewlyCoinedWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewlyCoinedWords
+//
+//  Created by SC on 2022/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
